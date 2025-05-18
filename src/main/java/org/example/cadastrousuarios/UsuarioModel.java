85ff7e085a46d6175fbf2e85c2dd3a5cc80e992f
gusta <gustavossantos@gmail.com>
2025-05-18 19:12:42 -0300

    Mudança no pom Alteração no Controller Criação do Model

---
+++ src/main/java/org/example/cadastrousuarios/UsuarioModel.java
@@ -0,0 +1,69 @@
+package org.example.cadastrousuarios;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+// Ele transforma uma classe em uma entidade no BD
+
+
+@Entity
+@Table(name = "tb_cadastro")
+public class UsuarioModel {
+    Long ID;
+    String nome;
+    String email;
+    int idade;
+    int telefone;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public UsuarioModel(int idade, String nome, String email, int telefone) {
+        this.idade = idade;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public UsuarioModel() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
