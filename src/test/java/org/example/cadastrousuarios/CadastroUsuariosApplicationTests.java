52cff3f0f93a2019695da01d78e6e848e553c154
gusta <gustavossantos@gmail.com>
2025-05-18 18:17:53 -0300

    Firt Commit

---
+++ src/test/java/org/example/cadastrousuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cadastrousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
